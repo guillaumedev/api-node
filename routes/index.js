87b4264e10427f4e47a7541937b93630dcd26353
Guillaume Brosse <darkedenzik@gmail.com>
2015-03-10 22:24:38 +0100

    testapi

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ var router = express.Router();
 // HOME PAGE ===========================
 // =====================================
 router.get('/', function(req, res) {
+	res.send({ test : "ok"});
 });
 
 module.exports = router;
